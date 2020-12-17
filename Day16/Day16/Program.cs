410b8f5e4b920322bf8879d54563d0e24cf12d1d
J D <jackdye201@hotmail.com>
2020-12-17 20:18:07 +0000

    Day 17 completed

---
+++ Day16/Day16/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    class Program
+    {
+        class MinMaxValidator : IValidationRule
+        {
+            public int Min;
+            public int Max;
+
+            public bool Validate(int n)
+            {
+                return (n >= Min && n <= Max);
+            }
+        }
+        class StackedMinMax : IValidationRule
+        {
+            public string FieldName;
+            public List<MinMaxValidator> List = new List<MinMaxValidator>();
+            public bool Validate(int input)
+            {
+                return List.Any(t => t.Validate(input));
+            }
+        }
+
+        interface IValidationRule
+        {
+            bool Validate(int input);
+        }
+
+        static void Main(string[] args)
+        {
+            Regex ruleParser = new Regex(@"^(.+):\s(\d+)-(\d+)\sor\s(\d+)-(\d+)$");
+            Console.WriteLine("Hello World!");
+            List<StackedMinMax> requirements = new List<StackedMinMax>();
+
+            List<int> myTicket = new List<int>();
+            List<List<int>> nearbyTickets = new List<List<int>>();
+            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
+            {
+                string line;
+                bool captureMyTicket = false;
+                bool captureNearbyTickets = false;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        if(captureMyTicket)
+                        {
+                            myTicket = line.Split(',').Select(t => int.Parse(t)).ToList();
+                            captureMyTicket = false;
+                        }
+                        else if(captureNearbyTickets)
+                        {
+                            nearbyTickets.Add(line.Split(',').Select(t => int.Parse(t)).ToList());
+                        }
+                        else if(line.StartsWith("your ticket:"))
+                        {
+                            captureMyTicket = true;
+                        }
+                        else if(line.StartsWith("nearby tickets:"))
+                        {
+                            captureNearbyTickets = true;
+                        }
+                        else
+                        {
+                            var l = ruleParser.Match(line);
+                            if (!l.Success)
+                                throw new Exception($"Failed to parse {line}");
+
+                            StackedMinMax smm = new StackedMinMax()
+                            {
+                                FieldName = l.Groups[1].Value,
+                                List = new List<MinMaxValidator>()
+                                {
+                                    new MinMaxValidator(){Min = int.Parse(l.Groups[2].Value), Max = int.Parse(l.Groups[3].Value)},
+                                    new MinMaxValidator(){Min = int.Parse(l.Groups[4].Value), Max = int.Parse(l.Groups[5].Value)}
+                                }
+                            };
+                            requirements.Add(smm);
+                        }
+                    }
+                }
+
+                int part1Total = 0;
+                List<List<int>> ticketsToDiscard = new List<List<int>>();
+                foreach(var ticket in nearbyTickets)
+                {
+                    foreach(var v in ticket)
+                    {
+                        if(requirements.All(t=> !t.Validate(v)))
+                        {
+                            Console.WriteLine($"Number: {v} is invalid for all rules");
+                            part1Total += v;
+                            ticketsToDiscard.Add(ticket);
+                            break;
+                        }
+                    }
+                }
+                nearbyTickets.RemoveAll(t=> ticketsToDiscard.Contains(t));
+                ticketsToDiscard.Clear();
+                Console.WriteLine($"Part1: {part1Total}");
+
+
+                //Part 2
+                List<List<StackedMinMax>> fieldMap = new List<List<StackedMinMax>>();
+                for (int i = 0; i < myTicket.Count; ++i)
+                {
+                    fieldMap.Add(requirements.ToList());    //Add all possible mappings
+                }
+
+
+                while (fieldMap.Any(t=>t.Count != 1))
+                {
+                    for (int i = 0; i < myTicket.Count; ++i)
+                    {
+                        foreach (var ticket in nearbyTickets.Concat(new List<List<int>> { myTicket }))
+                        {
+                            fieldMap[i].RemoveAll(t => !t.Validate(ticket[i]));
+                        }
+                        if (fieldMap[i].Count == 1)
+                        {
+                            var toDeleteFromEveryone = fieldMap[i].First();
+                            foreach(var spreadDelete in fieldMap.Where(t=>t != fieldMap[i]))
+                            {
+                                spreadDelete.Remove(toDeleteFromEveryone);  //knock out possible mappings from neighbors
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(string.Join(",", fieldMap.Select(t=>t.First().FieldName)));
+
+                long total = 1;
+                for(int i=0; i< myTicket.Count; ++i)
+                {
+                    if(fieldMap[i].First().FieldName.StartsWith("departure"))
+                    {
+                        total *= myTicket[i];
+                        Console.WriteLine($"{fieldMap[i].First().FieldName} = {myTicket[i]}");
+                    }
+                }
+                Console.WriteLine($"Part 2: {total}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Day17/Day17/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, bool> Space = new Dictionary<string, bool>();
+
+            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
+            {
+                string line = null;
+                int y = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int x = 0;  x < line.Length; ++x)
+                    {
+                        if (line[x] == '#')
+                            Space[CoordToString(x, y, 0)] = true;
+                    }
+                    y++;
+                }
+            }
+
+
+            for (int i = 0; i < 6; ++i)
+            {
+                Dictionary<string, bool> cloned = new Dictionary<string, bool>();
+
+                foreach (var d in Space)
+                {
+                    int activeNeighbours = 0;
+                    foreach (var neighbour in GetNeightbours(d.Key))
+                    {
+                        //Does this neighbor exist in our array, and can we affect it?
+                        if (!Space.ContainsKey(neighbour))
+                        {
+                            if (d.Value)    //(Dumass optimisation) This node cannot contribute to the neighbours so ignore it
+                            {
+                                int subactive = 0;
+                                foreach (var subNeighbour in GetNeightbours(neighbour))
+                                {
+                                    if (Space.ContainsKey(subNeighbour) && Space[subNeighbour])
+                                        subactive++;
+                                }
+                                if (subactive == 3)
+                                    cloned[neighbour] = true;
+                            }
+                        }
+                        else
+                        {
+                            if(Space[neighbour])
+                                activeNeighbours++;
+                        }
+                    }
+                    if(d.Value)
+                    {
+                        if (activeNeighbours == 2 || activeNeighbours == 3)
+                            cloned[d.Key] = true;
+                    }
+                    else
+                        if(activeNeighbours == 3)
+                            cloned[d.Key] = true;
+                }
+                Space = cloned;
+
+                Console.WriteLine($"Cycle {i}: {Space.Count(t => t.Value)}");
+            }
+            Console.WriteLine($"Part 1: {Space.Count(t => t.Value)}");
+
+            Space.Clear();
+            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
+            {
+                string line = null;
+                int y = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int x = 0; x < line.Length; ++x)
+                    {
+                        if (line[x] == '#')
+                            Space[CoordToString4(x, y, 0, 0)] = true;
+                    }
+                    y++;
+                }
+            }
+
+
+            for (int i = 0; i < 6; ++i)
+            {
+                Dictionary<string, bool> cloned = new Dictionary<string, bool>();
+
+                foreach (var d in Space)
+                {
+                    int activeNeighbours = 0;
+                    foreach (var neighbour in GetNeightbours4(d.Key))
+                    {
+                        //Does this neighbor exist in our array, and can we affect it?
+                        if (!Space.ContainsKey(neighbour))
+                        {
+                            if (d.Value)    //(Dumass optimisation) This node cannot contribute to the neighbours so ignore it
+                            {
+                                int subactive = 0;
+                                foreach (var subNeighbour in GetNeightbours4(neighbour))
+                                {
+                                    if (Space.ContainsKey(subNeighbour) && Space[subNeighbour])
+                                        subactive++;
+                                }
+                                if (subactive == 3)
+                                    cloned[neighbour] = true;
+                            }
+                        }
+                        else
+                        {
+                            if (Space[neighbour])
+                                activeNeighbours++;
+                        }
+                    }
+                    if (d.Value)
+                    {
+                        if (activeNeighbours == 2 || activeNeighbours == 3)
+                            cloned[d.Key] = true;
+                    }
+                    else
+                        if (activeNeighbours == 3)
+                            cloned[d.Key] = true;
+                }
+                Space = cloned;
+
+                Console.WriteLine($"Cycle {i}: {Space.Count(t => t.Value)}");
+            }
+            Console.WriteLine($"Part 2: {Space.Count(t => t.Value)}");
+            Console.ReadLine();
+        }
+
+
+        static string CoordToString(int x, int y, int z)
+        {
+            return x + "," + y + "," + z;
+        }
+
+        static IEnumerable<string> GetNeightbours(string coord)
+        {
+            var co = coord.Split(',').Select(t=> int.Parse(t)).ToList();
+
+            for(int z = -1; z < 2; ++z)
+            {
+                for (int y = -1; y < 2; ++y)
+                {
+                    for (int x = -1; x < 2; ++x)
+                    {
+                        if (x == 0 && y == 0 && z == 0)
+                            continue;
+
+                        yield return CoordToString(co[0] + x, co[1] + y, co[2] + z);
+                    }
+                }
+            }
+        }
+        static string CoordToString4(int x, int y, int z, int w)
+        {
+            return x + "," + y + "," + z + "," + w;
+        }
+
+        static IEnumerable<string> GetNeightbours4(string coord)
+        {
+            var co = coord.Split(',').Select(t => int.Parse(t)).ToList();
+            for (int w = -1; w < 2; ++w)
+            {
+                for (int z = -1; z < 2; ++z)
+                {
+                    for (int y = -1; y < 2; ++y)
+                    {
+                        for (int x = -1; x < 2; ++x)
+                        {
+                            if (x == 0 && y == 0 && z == 0 && w == 0)
+                                continue;
+
+                            yield return CoordToString4(co[0] + x, co[1] + y, co[2] + z, co[3] + w);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
